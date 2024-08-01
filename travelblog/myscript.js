d5ddc988c3165179a54e6c224993698a097de5f5
hollyelms <holly.elms@rspb.org.uk>
2024-08-01 12:47:36 +0100

    Add files via upload

---
+++ travelblog/myscript.js
@@ -0,0 +1,29 @@
+var name = "holly";
+let surname = "elms"
+var job = "Designer"
+const full = name + " " + surname
+
+function calcArea(width, height) {
+    let result = width * height;
+    return result;
+};
+
+function pressAlert() {
+    let result = width * height;
+    return result;
+};
+
+
+function pressAlert() {
+    console.log('Button has been pressed')
+    }
+    document.getElementById(“myButton”).addEventListener(“click”, );
+
+
+    
+function pressAlert() {
+        console.log('Button has been pressed')
+        }
+        
+        
+        ('myButton').addEventListener('click', pressAlert);
