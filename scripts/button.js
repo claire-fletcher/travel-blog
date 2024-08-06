778ff8c695535bb27d9ecb3e9adfc8ee412b02ba
claire-fletcher <claire.fletcher@pexip.com>
2024-08-06 21:34:50 +0100

    add some more renames and formatting for the layout of the files/folders

---
+++ scripts/button.js
@@ -0,0 +1,31 @@
+var name = "holly";
+let surname = "elms"
+var job = "Designer"
+const full = name + " " + surname
+
+function calcArea(width, height) {
+    let result = width * height;
+    return result;
+};
+
+function pressAlert() {
+    let result = width * height;
+    return result;
+};
+
+
+function pressAlert() {
+    console.log('Button has been pressed')
+}
+document.getElementById('myButton').addEventListener('click', pressAlert);
+
+
+
+function pressAlert() {
+    console.log('Button has been pressed')
+}
+
+
+('myButton').addEventListener('click', pressAlert);
+
+const carousel = new bootstrap.Carousel('#myCarousel')
